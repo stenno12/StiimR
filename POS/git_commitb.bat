3fbd53ba7c1830d19fb4987ace0e54d16486ab4c
Stenno Sirdla <stenno.sirdla@gmail.com>
2014-06-02 01:58:32 +0300

    testanta

--- POS/git_commitb.bat
+++ POS/git_commitb.bat
@@ -1,10 +1,12 @@
 
+			@echo off
 			setlocal 
 			path=C:\Users\Stenno\AppData\Local\GitHub\PortableGit_054f2e797ebafd44a30203088cd3d58663c627ef\bin;%path% 
 			cd C:\Users\Stenno\Desktop\git\Stiim
+			@echo on
 			git add -A
-			git commit -m k
-			git push https://stenno12:k@github.com/stenno12/StiimR.git
+			git commit -m testanta
+			git push https://stenno12:parool12@github.com/stenno12/StiimR.git
 			pause
 	   	
 	    
