005c268d37298591738e3f2058b54838f41e41c6
Stenno Sirdla <stenno.sirdla@gmail.com>
2014-06-02 02:12:29 +0300

    testll

--- POS/git_commitb.bat
+++ POS/git_commitb.bat
@@ -1,12 +1,12 @@
 
 			@echo off
 			setlocal 
-			path=C:\Users\Stenno\AppData\Local\GitHub\PortableGit_054f2e797ebafd44a30203088cd3d58663c627ef\bin;%path% 
+			path=C:\Users\Stenno\AppData\Local\GitHub\PortableGit_054f2e797ebafd44a30203088cd3d58663c627ef\bin;
+			path=C:\Users\Stenno\AppData\Local\GitHub\PortableGit_054f2e797ebafd44a30203088cd3d58663c627ef\cmd;%path% 
 			cd C:\Users\Stenno\Desktop\git\Stiim
 			@echo on
 			git add -A
-			git commit -m testanta
+			git commit -m testll
 			git push https://stenno12:parool12@github.com/stenno12/StiimR.git
-			pause
 	   	
 	    
