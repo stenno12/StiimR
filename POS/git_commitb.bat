6cbf5dc07004ad166d2e76fa2adb9a1bf0bec243
Stenno Sirdla <stenno.sirdla@gmail.com>
2014-06-02 01:17:34 +0300

    k

--- POS/git_commitb.bat
+++ POS/git_commitb.bat
@@ -1,7 +1,8 @@
 
 			cd C:\Users\Stenno\Desktop\git\Stiim
 			git add -A
-			git commit -m f
-			git push https://stenno12:f@github.com/stenno12/StiimR.git
+			git commit -m k
+			git push https://stenno12:k@github.com/stenno12/StiimR.git
+			pause
 	   	
 	    
