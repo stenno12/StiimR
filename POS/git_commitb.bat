4fae294bf55afef8a539480d963c0d03876a9bf4
Stenno Sirdla <stenno.sirdla@gmail.com>
2014-06-02 01:55:24 +0300

    k

--- POS/git_commitb.bat
+++ POS/git_commitb.bat
@@ -1,4 +1,6 @@
 
+			setlocal 
+			path=C:\Users\Stenno\AppData\Local\GitHub\PortableGit_054f2e797ebafd44a30203088cd3d58663c627ef\bin;%path% 
 			cd C:\Users\Stenno\Desktop\git\Stiim
 			git add -A
 			git commit -m k
